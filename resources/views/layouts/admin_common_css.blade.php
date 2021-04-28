83905c511615722430555123ef3c599afb2e1cb9
= <jira.programmers@gmail.com>
2021-04-28 22:53:03 +0600

    Removed unwanted file

--- resources/views/layouts/admin_common_css.blade.php
+++
@@ -1,5 +0,0 @@
-
-
-<link rel="stylesheet" type="text/css" href="{{asset('admin_src/plugins/fontawesome-free/css/all.min.css')}}">
-<link rel="stylesheet" type="text/css" href="{{asset('admin_src/dist/css/adminlte.min.css')}}">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
--- resources/views/layouts/admin_common_js.blade.php
+++
@@ -1,4 +0,0 @@
-
-<script src="{{asset('admin_src/plugins/jquery/jquery.min.js')}}"></script>
-<script src="{{asset('admin_src/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{asset('admin_src/dist/js/adminlte.min.js')}}"></script>
