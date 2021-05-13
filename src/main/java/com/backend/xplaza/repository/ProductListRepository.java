402a6c6d117412f9c589f2b2b47254684d125966
Mahiudidin Al Kamal <mhak@mms-t-systems.de>
2021-05-13 11:17:12 +0200

    fixed query bug

--- src/main/java/com/backend/xplaza/repository/ProductListRepository.java
+++ src/main/java/com/backend/xplaza/repository/ProductListRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
 public interface ProductListRepository extends JpaRepository<ProductList, Long> {
-    @Query(value = "select p.*, b.brand_name, s.shop_name, c.category_name, var.product_var_type_name, cur.currency_name from products p " +
+    @Query(value = "select p.*, b.brand_name, s.shop_name, c.category_name, var.var_type_name, cur.currency_name from products p " +
             "left join brands b on p.fk_brand_id = b.brand_id " +
             "left join shops s on p.fk_shop_id = s.shop_id " +
             "left join categories c on p.fk_category_id = c.category_id " +
@@ -16,7 +16,7 @@ public interface ProductListRepository extends JpaRepository<ProductList, Long> 
             "where product_id = ?1", nativeQuery = true)
     ProductList findItemById(Long id);
 
-    @Query(value = "select p.*, b.brand_name, s.shop_name, c.category_name, var.product_var_type_name, cur.currency_name from products p " +
+    @Query(value = "select p.*, b.brand_name, s.shop_name, c.category_name, var.var_type_name, cur.currency_name from products p " +
             "left join brands b on p.fk_brand_id = b.brand_id " +
             "left join shops s on p.fk_shop_id = s.shop_id " +
             "left join categories c on p.fk_category_id = c.category_id " +
