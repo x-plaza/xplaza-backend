2498b8793fe2a4f755a7770b8108ea16fb52e6b9
Mahiudidin Al Kamal <mhak@mms-t-systems.de>
2021-07-28 23:00:03 +0200

    fixed bug

--- src/main/java/com/backend/xplaza/model/ProductDiscountList.java
+++ src/main/java/com/backend/xplaza/model/ProductDiscountList.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import org.hibernate.annotations.TypeDef;
 
 import javax.persistence.*;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Data
@@ -50,8 +51,18 @@ public class ProductDiscountList {
     @Column(name="discount_start_date")
     private Date start_date;
 
+    public String getStart_date() {
+        if(start_date != null) return new SimpleDateFormat("dd MMM yyyy HH:mm:ss").format(start_date);
+        return null;
+    }
+
     @Column(name="discount_end_date")
     private Date end_date;
+
+    public String getEnd_date() {
+        if(end_date != null) return new SimpleDateFormat("dd MMM yyyy HH:mm:ss").format(end_date);
+        return null;
+    }
 
     public ProductDiscountList() {}
 }
