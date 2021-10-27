813bec02d8cdce7f8baacef509980cee62326271
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2021-10-27 20:55:47 +0200

    fixed a bug

--- src/main/java/com/backend/xplaza/repository/CustomerLoginRepository.java
+++ src/main/java/com/backend/xplaza/repository/CustomerLoginRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 public interface CustomerLoginRepository extends JpaRepository<CustomerLogin, Long> {
-    @Query(value = "select customer_id, concat(first_name,' ', last_name) as customer_name, email " +
+    @Query(value = "select customer_id, concat(first_name,' ', last_name) as customer_name, email, false as authentication  " +
             "from customers where email = ?1", nativeQuery = true)
     CustomerLogin findCustomerByUsername(String username);
 }
