3152932ebb9d28df11fbf15dcb8cc3f896741215
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2021-11-20 18:25:30 +0100

    build dependency added for fat jar

--- src/main/java/com/backend/xplaza/XplazaApplication.java
+++ src/main/java/com/backend/xplaza/XplazaApplication.java
@@ -1,11 +1,13 @@
 package com.backend.xplaza;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
+@EnableAutoConfiguration
 public class XplazaApplication {
 
 	public static void main(String[] args) {
