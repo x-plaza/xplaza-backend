57d0756d058772a1b39e88dbf716493ddd63c3b2
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2025-05-27 23:08:12 +0200

    fixed issues

--- src/main/java/com/xplaza/backend/model/ACL.java
+++ src/main/java/com/xplaza/backend/model/ACL.java
@@ -7,8 +7,12 @@ package com.xplaza.backend.model;
 import jakarta.persistence.Embeddable;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Embeddable
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/com/xplaza/backend/repository/ProductSearchRepository.java
+++ src/main/java/com/xplaza/backend/repository/ProductSearchRepository.java
@@ -13,7 +13,7 @@ import com.xplaza.backend.model.ProductSearch;
 
 public interface ProductSearchRepository extends JpaRepository<ProductSearch, Long> {
   @Query(value = "select p.product_id, p.product_name from products p \n" +
-      "where p.fk_shop_id = ?1 and p.product_name iLIKE %?2% \n" +
+      "where p.fk_shop_id = ?1 and LOWER(p.product_name) LIKE LOWER(CONCAT('%', ?2, '%')) \n" +
       "ORDER BY p.product_name", nativeQuery = true)
   List<ProductSearch> findProductListByName(Long shop_id, String product_name);
 }
