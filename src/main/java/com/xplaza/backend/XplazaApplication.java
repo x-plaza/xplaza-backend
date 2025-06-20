ba40727dfa3c4271b7b56609d8b1cf7ba5b366b7
Mahiuddin Al Kamal <mahiuddinalkamal@gmail.com>
2025-06-20 23:46:19 +0200

    Update to java v24 (#38)

--- src/main/java/com/xplaza/backend/XplazaApplication.java
+++ src/main/java/com/xplaza/backend/XplazaApplication.java
@@ -5,13 +5,11 @@
 package com.xplaza.backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
-@EnableAutoConfiguration
 public class XplazaApplication {
   public static void main(String[] args) {
     SpringApplication.run(XplazaApplication.class, args);
